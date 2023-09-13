7eb971ed980642a25d0561b3641b5b1f3dee2d54
Tom Montgomery <montgomery3681@gmail.com>
2023-09-12 23:00:14 -0600

    Initial Commit

---
+++ Design_M1L5_TextFieldStyles/Design_M1L5_TextFieldStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M1L5_TextFieldStylesApp.swift
+//  Design_M1L5_TextFieldStyles
+//
+//  Created by tom montgomery on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M1L5_TextFieldStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
